e4c2bd761d43df00598e2dc2d0b27cf7f6d34aa1
sudoofus <daz.chambers@gmail.com>
2022-01-26 19:16:03 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='cifscloak',
-    version='1.0.24',
+    version='1.0.25',
     description='Mount cifs shares using encrypted passwords',
     long_description=long_description,
     long_description_content_type='text/markdown',
