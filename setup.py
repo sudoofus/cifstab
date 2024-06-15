f0ca628e429bcce94ade864de828941ff7ba95eb
root <root@localhost.localdomain>
2024-06-15 16:08:32 +0100

    Rename from cifscloak to cifstab

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ import os, stat
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
-    name='cifscloak',
-    version='1.0.34',
+    name='cifstab',
+    version='1.0.35',
     description='Mount cifs shares using encrypted passwords',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Darren Chambers',
     author_email='dazchambers@gmail.com',
-    url='https://github.com/sudoofus/cifscloak',
+    url='https://github.com/sudoofus/cifstab',
     packages=setuptools.find_packages(),
     install_requires=[
         'cryptography',
@@ -20,5 +21,5 @@ setuptools.setup(
         'pexpect',
         'regex'
         ],
-    entry_points = { 'console_scripts':['cifscloak = cifscloak.cifscloak:main', 'cifsfs = cifscloak.cifscloak:main'] }
+    entry_points = { 'console_scripts':['cifscloak = cifstab.cifstab:main', 'cifstab = cifstab.cifstab:main'] }
 )
